c123ee7b2854aa4abf276478ada951bfb5d18e4e
TheAndyPi <anderson.chan@stonybrook.edu>
2026-08-05 15:56:34 -0400

    Revised online batch script

--- RunLaptopExport-v0.8.bat
+++ RunLaptopExport-v0.8.bat
@@ -7,7 +7,7 @@ setlocal
 set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_PATH=%SCRIPT_DIR%Export-LaptopData-v0.8.ps1"
 set "TEMP_PATH=%SCRIPT_PATH%.download"
-set "SCRIPT_URL=https://raw.githubusercontent.com/TheAndyPi/Laptop-Export/A1A2-Prototype/Export-LaptopData.ps1"
+set "SCRIPT_URL=https://raw.githubusercontent.com/TheAndyPi/Laptop-Export/main/Export-LaptopData.ps1"
 
 echo Downloading the latest Export-LaptopData.ps1...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "try { Invoke-WebRequest -UseBasicParsing -Uri '%SCRIPT_URL%' -OutFile '%TEMP_PATH%' -ErrorAction Stop; Move-Item -LiteralPath '%TEMP_PATH%' -Destination '%SCRIPT_PATH%' -Force -ErrorAction Stop } catch { Write-Error $_; exit 1 }"
